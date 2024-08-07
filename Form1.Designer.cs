521cae3650efe6750a03078e4879b7c80ea82ce9
joaopedroAMO <156593886+joaopedroAMO@users.noreply.github.com>
2024-08-07 19:09:30 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTarefa = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.listTodo = new System.Windows.Forms.ListBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite Sua Tarefa";
+            // 
+            // txtTarefa
+            // 
+            this.txtTarefa.Location = new System.Drawing.Point(47, 137);
+            this.txtTarefa.Multiline = true;
+            this.txtTarefa.Name = "txtTarefa";
+            this.txtTarefa.Size = new System.Drawing.Size(322, 35);
+            this.txtTarefa.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(47, 202);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 38);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(157, 200);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(91, 43);
+            this.btnEditar.TabIndex = 4;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(272, 202);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(97, 43);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // listTodo
+            // 
+            this.listTodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listTodo.FormattingEnabled = true;
+            this.listTodo.ItemHeight = 20;
+            this.listTodo.Location = new System.Drawing.Point(411, 107);
+            this.listTodo.Name = "listTodo";
+            this.listTodo.Size = new System.Drawing.Size(418, 244);
+            this.listTodo.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSave.Location = new System.Drawing.Point(47, 202);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(88, 38);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Visible = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(47, 257);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(90, 43);
+            this.btnFechar.TabIndex = 8;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.ForeColor = System.Drawing.Color.Red;
+            this.btnCancelar.Location = new System.Drawing.Point(157, 202);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(91, 43);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Visible = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(881, 569);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.listTodo);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTarefa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "To Do List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTarefa;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.ListBox listTodo;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtTarefa.Text))
+            {
+                listTodo.Items.Add(txtTarefa.Text);
+                txtTarefa.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Porfavor digite algo valido");
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (listTodo.SelectedItem != null)
+            {
+                txtTarefa.Focus();
+                btnSave.Visible = true;
+                btnAdd.Visible = false;
+                btnCancelar.Visible = true;
+                btnEditar.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("por favor selecione uma tarefa para editar");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrWhiteSpace(txtTarefa.Text))
+            {
+                int selectedItem = listTodo.SelectedIndex;
+                listTodo.Items[selectedItem] = txtTarefa.Text;
+
+                txtTarefa.Clear();
+                txtTarefa.Focus();
+
+                btnSave.Visible = false;
+                btnAdd.Visible = true;
+                btnCancelar.Visible = false;
+                btnEditar.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Por favor digite algo valido para salvar");
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (listTodo.SelectedItem != null)
+            {
+                listTodo.Items.Remove(listTodo.SelectedItem);
+                txtTarefa.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecione uma tarefa para excluir");
+            }
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtTarefa.Focus();
+            txtTarefa.Clear();
+            btnSave.Visible = false;
+            btnAdd.Visible = true;
+            btnCancelar.Visible = false;
+            btnEditar.Visible = true;
+        }
+    }
+}
